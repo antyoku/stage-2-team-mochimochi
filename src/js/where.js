32f9057af63d8bbf6083bfa680950c40cb15f8b3
antyoku <antyokunaoyasu@gmail.com>
2018-03-26 21:33:44 +0900

    仕様上、入力画面にマーカーは必要ないことに気づいたので削除。

--- src/js/where.js
+++ src/js/where.js
@@ -1,24 +1,5 @@
 'use strict'
 
-var markerWidth
-var markerHeight
-
-if (window.innerWidth < 600) {
-  markerWidth = 18.59;
-  markerHeight = 30.86;
-} else {
-  markerWidth = 45;
-  markerHeight = 74.68;
-};
-
-var markerImg = new google.maps.MarkerImage(
-  "image/map-marker.svg",
-  new google.maps.Size(100, 100),
-  new google.maps.Point(0, 0),
-  new google.maps.Point(markerWidth/2, markerHeight),
-  new google.maps.Size(markerWidth, markerHeight)
-);
-
 var styleOptions = [{
     "elementType": "geometry",
     "stylers": [{
@@ -226,13 +207,7 @@ var getMap = (function () {
           zoom: 8,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         });
-
         map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location,
-          icon: markerImg
-        });
         map.setOptions({
           styles: styleOptions
         });
