c5b836609e24c6961c2e87003c946cccd04f984a
antyoku <antyokunaoyasu@gmail.com>
2018-03-30 15:37:00 +0900

    入力内容を削除すると画像も消えるよう修正 #18

--- src/js/favorite.js
+++ src/js/favorite.js
@@ -1,20 +1,21 @@
 'use strict'
 
-
 $('#fav-input-id').change(function () {
   var urlFav = document.getElementById('fav-input-id').value;
   if (urlFav.match(/watch/)) {
     var idMovie = urlFav.split(/.*v=([\d\w\-]+).*/);
     var output = document.getElementById('output');
-    output.innerHTML = "<img class=\"favorite__thumbnail\" src='https://img.youtube.com/vi/"+idMovie[1]+"/sddefault.jpg' alt=\"\">";
-    $('.favorite__thumbnail').fadeIn();
+    output.innerHTML = "<img class=\"favorite__thumbnail-img\" src='https://img.youtube.com/vi/" + idMovie[1] + "/sddefault.jpg' alt=\"\">";
+    $('.favorite__thumbnail-img').fadeIn();
   } else if (urlFav.match(/youtu.be/)) {
     var idMovie = urlFav.split(/.*\.be\/([\d\w\-]+).*/);
     var output = document.getElementById('output');
-    output.innerHTML = "<img class=\"favorite__thumbnail\" src='https://img.youtube.com/vi/"+idMovie[1]+"/default.jpg' alt=\"\">";
+    output.innerHTML = "<img class=\"favorite__thumbnail-img\" src='https://img.youtube.com/vi/" + idMovie[1] + "/default.jpg' alt=\"\">";
   } else if (urlFav.match(/embed/)) {
     var idMovie = urlFav.split(/.*\embed\/([\d\w\-]+).*/);
     var output = document.getElementById('output');
-    output.innerHTML = "<img class=\"favorite__thumbnail\" src='https://img.youtube.com/vi/"+idMovie[1]+"/default.jpg' alt=\"\">";    
+    output.innerHTML = "<img class=\"favorite__thumbnail-img\" src='https://img.youtube.com/vi/" + idMovie[1] + "/default.jpg' alt=\"\">";
+  } else {
+    $('.favorite__thumbnail-img').remove();
   }
 });
