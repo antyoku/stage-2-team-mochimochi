20e38b44049adb4f63680a6b6bcba8d332f4a85d
antyoku <antyokunaoyasu@gmail.com>
2018-03-30 13:48:44 +0900

    画像の位置調整、明度を暗く #18

--- src/js/favorite.js
+++ src/js/favorite.js
@@ -6,14 +6,15 @@ $('#fav-input-id').change(function () {
   if (urlFav.match(/watch/)) {
     var idMovie = urlFav.split(/.*v=([\d\w\-]+).*/);
     var output = document.getElementById('output');
-    output.innerHTML = "<img src='https://img.youtube.com/vi/"+idMovie[1]+"/default.jpg'>";
+    output.innerHTML = "<img class=\"favorite__thumbnail\" src='https://img.youtube.com/vi/"+idMovie[1]+"/sddefault.jpg' alt=\"\">";
+    $('.favorite__thumbnail').fadeIn();
   } else if (urlFav.match(/youtu.be/)) {
     var idMovie = urlFav.split(/.*\.be\/([\d\w\-]+).*/);
     var output = document.getElementById('output');
-    output.innerHTML = "<img src='https://img.youtube.com/vi/"+idMovie[1]+"/default.jpg'>";
+    output.innerHTML = "<img class=\"favorite__thumbnail\" src='https://img.youtube.com/vi/"+idMovie[1]+"/default.jpg' alt=\"\">";
   } else if (urlFav.match(/embed/)) {
     var idMovie = urlFav.split(/.*\embed\/([\d\w\-]+).*/);
     var output = document.getElementById('output');
-    output.innerHTML = "<img src='https://img.youtube.com/vi/"+idMovie[1]+"/default.jpg'>";    
+    output.innerHTML = "<img class=\"favorite__thumbnail\" src='https://img.youtube.com/vi/"+idMovie[1]+"/default.jpg' alt=\"\">";    
   }
 });
