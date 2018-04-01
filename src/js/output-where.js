0a221707874565acc6a97728b48ccba652242097
antyoku <antyokunaoyasu@gmail.com>
2018-04-01 19:00:14 +0900

    エラーメッセージが必要ないので削除 #29

--- src/js/output-where.js
+++ src/js/output-where.js
@@ -5,39 +5,35 @@ function outputWhere(outputAddress) {
   geocoder.geocode({
     'address': outputAddress
   }, function (results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      var map = new google.maps.Map(document.getElementById('map-area'), {
-        zoom: 8,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        disableDefaultUI: true
-      });
-      map.setCenter(results[0].geometry.location);
-      var markerWidth;
-      var markerHeight;
-      if (window.innerWidth < 600) {
-        markerWidth = 18.59;
-        markerHeight = 30.86;
-      } else {
-        markerWidth = 45;
-        markerHeight = 74.68;
-      };
-      var markerImg = new google.maps.MarkerImage(
-        "image/map-marker.svg",
-        new google.maps.Size(100, 100),
-        new google.maps.Point(0, 0),
-        new google.maps.Point(markerWidth / 2, markerHeight),
-        new google.maps.Size(markerWidth, markerHeight)
-      );
-      var marker = new google.maps.Marker({
-        map: map,
-        position: results[0].geometry.location,
-        icon: markerImg
-      });
-      map.setOptions({
-        styles: styleOptions
-      });
+    var map = new google.maps.Map(document.getElementById('map-area'), {
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: true
+    });
+    map.setCenter(results[0].geometry.location);
+    var markerWidth;
+    var markerHeight;
+    if (window.innerWidth < 600) {
+      markerWidth = 18.59;
+      markerHeight = 30.86;
     } else {
-      console.log('Geocode was not successful for the following reason: ' + status);
-    }
+      markerWidth = 45;
+      markerHeight = 74.68;
+    };
+    var markerImg = new google.maps.MarkerImage(
+      "image/map-marker.svg",
+      new google.maps.Size(100, 100),
+      new google.maps.Point(0, 0),
+      new google.maps.Point(markerWidth / 2, markerHeight),
+      new google.maps.Size(markerWidth, markerHeight)
+    );
+    var marker = new google.maps.Marker({
+      map: map,
+      position: results[0].geometry.location,
+      icon: markerImg
+    });
+    map.setOptions({
+      styles: styleOptions
+    });
   });
 }
