c9a80bf67cc8df1af2a1dabc7be2a33916880655
mochimochimoon <mochi.mochi.moon5098@gmail.com>
2018-04-03 23:28:30 +0900

    feat(create-btn.js):値を取得し、ページ移動する機能を作成 #46

---
+++ src/js/create-btn.js
@@ -0,0 +1,31 @@
+/* global $ */
+$('#js-create').on('click', function () {
+  var year, month, day, sex, what, where, favorite,
+    param
+
+  // inputの値を変数に格納
+  year = $('.profile__birth-year').val()
+  month = $('.profile__birth-month').val()
+  day = $('.profile__birth-day').val()
+  sex = (function () {
+    if ($('#male').val() === 'on') {
+      return 'male'
+    } else if ($('#female').val() === 'on') {
+      return 'female'
+    } else {
+      return null
+    }
+  })()
+  what = encodeURIComponent($('#js-input-what').val())
+  where = $('#where-input-id').val()
+  favorite = encodeURIComponent($('#fav-input-id').val())
+
+  // 全て入力しているか判定
+  if (year && month && day && sex && what && where && favorite) {
+    // URL+パラメータに移動
+    param = '?bday=' + year + '-' + month + '-' + day + '&gender=' + sex + '&city=' + where + '&youtube=' + favorite + '&wau=' + what
+    window.location.href = window.location.href.slice(0, window.location.search.length * -1) + param
+  } else {
+    window.alert('未入力の項目があります。')
+  }
+});
