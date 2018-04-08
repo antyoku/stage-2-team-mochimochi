bdd66214769a690a6677e9b4b99185dcaa8e71e2
TagYut <Tagami.Yutaro@gmail.com>
2018-04-08 22:00:43 +0900

    issue No.81,84を修正(mixin化,btnをfavorite-btn--focusに) #115

--- src/js/favorite.js
+++ src/js/favorite.js
@@ -22,13 +22,13 @@ $('#fav-input-id').change(function () {
   }
 });
 
-$('.focus').focus(function () {
+$('.favorite-btn--focus').focus(function () {
   if (this.value === 'https://www.youtube.com/～') {
     $(this).val('').css('color', '#707070');
   }
 });
 
-$('.focus').blur(function () {
+$('.favorite-btn--focus').blur(function () {
   if (this.value === '') {
     $(this).val('https://www.youtube.com/～').css('color', '#353535');
   }
