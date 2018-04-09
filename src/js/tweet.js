34fe09f6903aec289d1a9526ef328832c36ee326
mochimochimoon <mochi.mochi.moon5098@gmail.com>
2018-04-09 16:35:33 +0900

    refactor(tweet.js):関数を廃止。コードを整列 #122

--- src/js/tweet.js
+++ src/js/tweet.js
@@ -1,11 +1,11 @@
 /* global gapi */
 /* global $ */
-
-// a#js-tweetのhrefを書き換える
 (function () {
+  // a#js-tweetのhrefを書き換える
   window.addEventListener('load', function () {
     shortener(window.location.href)
   })
+
   function shortener (url) {
     gapi.client.setApiKey('AIzaSyCubhNb4Ace1iEMQAJuoUQ07-6JXAZUlsg')
     gapi.client.load('urlshortener', 'v1', function () {
@@ -15,13 +15,10 @@
         }
       })
       request.execute(function (response) {
-        changeHref(response.id)
+        $('#js-tweet').attr({
+          href: 'https://twitter.com/intent/tweet' + '/?text=' + response.id
+        })
       })
     })
   }
-  function changeHref (shortenUrl) {
-    $('#js-tweet').attr({
-      href: 'https://twitter.com/intent/tweet' + '/?text=' + shortenUrl
-    })
-  }
-})()
+})();
