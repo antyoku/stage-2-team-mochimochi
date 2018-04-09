6d75d20d998a8e2765d7aaea0ed0fbfcefcab158
mochimochimoon <mochi.mochi.moon5098@gmail.com>
2018-04-09 15:23:06 +0900

    feat(what.js):コードを即時関数で囲む #127

--- src/js/what.js
+++ src/js/what.js
@@ -1,5 +1,7 @@
 /* global $ */
-$('#js-input-what').on('click change keyup', function () {
-  var inputLength = this.value.length
-  $('#js-input-what-length').text(inputLength <= 10 ? inputLength : 10)
-})
+(function () {
+  $('#js-input-what').on('click change keyup', function () {
+    var inputLength = this.value.length
+    $('#js-input-what-length').text(inputLength <= 10 ? inputLength : 10)
+  })
+})();
