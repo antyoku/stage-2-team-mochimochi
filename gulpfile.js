80bfc8452bfed211726f9fec7ba8926486e6c8f8
mochimochimoon <mochi.mochi.moon5098@gmail.com>
2018-04-10 13:15:09 +0900

    chore(gulpfile.js):ESLintに沿った記法に変更 #107

--- gulpfile.js
+++ gulpfile.js
@@ -1,77 +1,76 @@
-var gulp = require('gulp'),
-  watch = require('gulp-watch'),
-  sass = require('gulp-sass'),
-  plumber = require('gulp-plumber'),
-  imagemin = require('gulp-imagemin'),
-  concat = require('gulp-concat'),
-  ejs = require('gulp-ejs'),
-  rename = require('gulp-rename'),
-  browserSync = require('browser-sync');
+var gulp = require('gulp')
+var watch = require('gulp-watch')
+var sass = require('gulp-sass')
+var plumber = require('gulp-plumber')
+var imagemin = require('gulp-imagemin')
+var concat = require('gulp-concat')
+var ejs = require('gulp-ejs')
+var rename = require('gulp-rename')
+var browserSync = require('browser-sync')
 
 gulp.task('server', function () {
   browserSync({
     server: {
-      baseDir: "./docs/",
+      baseDir: './docs/',
       startPath: './docs/index.html'
     }
-  });
+  })
   return watch('./docs/**', function () {
-    browserSync.reload();
-  });
-});
+    browserSync.reload()
+  })
+})
 
-//func-srcファイルをdocsへコピー
-function sassDocs() {
+// func-srcファイルをdocsへコピー
+function sassDocs () {
   gulp.src('./src/sass/core/*.scss')
     .pipe(plumber())
     .pipe(sass())
-    .pipe(gulp.dest('./docs/css'));
-};
+    .pipe(gulp.dest('./docs/css'))
+}
 
-function ejsDocs() {
+function ejsDocs () {
   gulp.src('./src/ejs/core/*.ejs')
     .pipe(plumber())
     .pipe(ejs())
     .pipe(rename({
       extname: '.html'
     }))
-    .pipe(gulp.dest('./docs'));
-};
+    .pipe(gulp.dest('./docs'))
+}
 
-function jsDocs() {
+function jsDocs () {
   gulp.src('./src/js/*.js')
     .pipe(concat('index.js'))
-    .pipe(gulp.dest('./docs/js'));
-};
+    .pipe(gulp.dest('./docs/js'))
+}
 
-function imageDocs() {
+function imageDocs () {
   gulp.src('./src/image/*')
     .pipe(plumber())
     .pipe(imagemin())
-    .pipe(gulp.dest('./docs/image'));
-};
+    .pipe(gulp.dest('./docs/image'))
+}
 
-//task-srcの変更を監視
+// task-srcの変更を監視
 gulp.task('watchSass', function () {
-  return watch('./src/sass/*/*.scss', sassDocs);
-});
+  return watch('./src/sass/*/*.scss', sassDocs)
+})
 gulp.task('watchEjs', function () {
-  return watch('./src/ejs/*/*.ejs', ejsDocs);
-});
+  return watch('./src/ejs/*/*.ejs', ejsDocs)
+})
 gulp.task('watchJs', function () {
-  return watch('./src/js/*.js', jsDocs);
-});
+  return watch('./src/js/*.js', jsDocs)
+})
 gulp.task('watchImage', function () {
-  return watch('./src/image/*', imageDocs);
-});
+  return watch('./src/image/*', imageDocs)
+})
 
-//task-srcを元にdocsを更新
+// task-srcを元にdocsを更新
 gulp.task('reloadDocs', function () {
-  sassDocs();
-  ejsDocs();
-  jsDocs();
-  imageDocs();
-});
+  sassDocs()
+  ejsDocs()
+  jsDocs()
+  imageDocs()
+})
 
-
-gulp.task('default', ['server', 'watchSass', 'watchEjs', 'watchJs', 'watchImage']);
+gulp.task('default', ['server', 'watchSass', 'watchEjs', 'watchJs', 'watchImage'])
